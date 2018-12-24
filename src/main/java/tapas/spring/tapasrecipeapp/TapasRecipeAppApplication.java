ed2691d3293e499ec2f726995e9afc4e48d84c15
tapas.satpathy <tapas.satpathy@gmail.com>
2018-12-24 15:07:38 +0900

    Committing and creating the basic recipe project.

---
+++ src/main/java/tapas/spring/tapasrecipeapp/TapasRecipeAppApplication.java
@@ -0,0 +1,14 @@
+package tapas.spring.tapasrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapasRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapasRecipeAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/tapas/spring/tapasrecipeapp/TapasRecipeAppApplicationTests.java
@@ -0,0 +1,17 @@
+package tapas.spring.tapasrecipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TapasRecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
